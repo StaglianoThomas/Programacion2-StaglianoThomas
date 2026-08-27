ce150f506da818ec10f11ca7deeba338c4db929f
Stagliano Thomas Agustin <stagliano.cufa@gmail.com>
2026-08-27 02:28:17 -0300

    ejercicio 1 t3
    Added header comments with Java version information.

--- Clase1/Tema3_Ej1.java
+++ Clase1/Tema3_Ej1.java
@@ -1,1 +1,15 @@
+/*
+javac -version:
+javac 26.0.2.1
 
+java -version:
+java version "26.0.2.1" 2026-08-18
+Java(TM) SE Runtime Environment (build 26.0.2.1+1-7)
+Java HotSpot(TM) 64-Bit Server VM (build 26.0.2.1+1-7, mixed mode, sharing)
+ */
+
+public class HolaProgramacionII {
+    public static void main(String[] args){
+        System.out.println("Hola Crack");
+    }
+}
